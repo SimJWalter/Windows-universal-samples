325aaa0d50d1a54239320b5f08cee01b993ad210
John Walter <jsmwalter.prodev@gmail.com>
2021-07-10 11:45:31 +0200

    comment out ambiguous code

--- SharedContent/cs/App.xaml.cs
+++ SharedContent/cs/App.xaml.cs
@@ -19,109 +19,109 @@ using Windows.UI.Xaml.Navigation;
 
 namespace SDKTemplate
 {
+  /// <summary>
+  /// Provides application-specific behavior to supplement the default Application class.
+  /// </summary>
+  sealed partial class App : Application
+  {
     /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
-    sealed partial class App : Application
+    public App()
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-            this.Construct();
-        }
+      this.InitializeComponent();
+      this.Construct();
+    }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
+    /// <summary>
+    /// Invoked when the application is launched normally by the end user.  Other entry points
+    /// will be used such as when the application is launched to open a specific file.
+    /// </summary>
+    /// <param name="e">Details about the launch request and process.</param>
+    protected override void OnLaunched(LaunchActivatedEventArgs e)
+    {
 
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = false;
-            }
+      if (System.Diagnostics.Debugger.IsAttached)
+      {
+        this.DebugSettings.EnableFrameRateCounter = false;
+      }
 #endif
 
-            Frame rootFrame = Window.Current.Content as Frame;
+      Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-                // Set the default language
-                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
+      // Do not repeat app initialization when the Window already has content,
+      // just ensure that the window is active
+      if (rootFrame == null)
+      {
+        // Create a Frame to act as the navigation context and navigate to the first page
+        rootFrame = new Frame();
+        // Set the default language
+        rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
+        rootFrame.NavigationFailed += OnNavigationFailed;
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
-            }
-
-            // Ensure the current window is active
-            Window.Current.Activate();
-
-            // Some samples want access to the LaunchActivatedEventArgs.
-            LaunchCompleted(e);
-        }
-
-        private Frame CreateRootFrame()
+        if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                // Set the default language
-                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
-                rootFrame.NavigationFailed += OnNavigationFailed;
-                
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            return rootFrame;
+          //TODO: Load state from previously suspended application
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
+        // Place the frame in the current Window
+        Window.Current.Content = rootFrame;
+      }
 
-        // Add any application contructor code in here.
-        partial void Construct();
+      if (rootFrame.Content == null)
+      {
+        // When the navigation stack isn't restored navigate to the first page,
+        // configuring the new page by passing required information as a navigation
+        // parameter
+        rootFrame.Navigate(typeof(MainPage), e.Arguments);
+      }
 
-        // Add any OnLaunched customization here.
-        partial void LaunchCompleted(LaunchActivatedEventArgs e);
+      // Ensure the current window is active
+      Window.Current.Activate();
+
+      // Some samples want access to the LaunchActivatedEventArgs.
+      LaunchCompleted(e);
     }
+
+    //private Frame CreateRootFrame()
+    //{
+    //    Frame rootFrame = Window.Current.Content as Frame;
+
+    //    // Do not repeat app initialization when the Window already has content,
+    //    // just ensure that the window is active
+    //    if (rootFrame == null)
+    //    {
+    //        // Create a Frame to act as the navigation context and navigate to the first page
+    //        rootFrame = new Frame();
+
+    //        // Set the default language
+    //        rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
+    //        rootFrame.NavigationFailed += OnNavigationFailed;
+
+    //        // Place the frame in the current Window
+    //        Window.Current.Content = rootFrame;
+    //    }
+
+    //    return rootFrame;
+    //}
+
+    /// <summary>
+    /// Invoked when Navigation to a certain page fails
+    /// </summary>
+    /// <param name="sender">The Frame which failed navigation</param>
+    /// <param name="e">Details about the navigation failure</param>
+    void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+    {
+      throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+    }
+
+    // Add any application contructor code in here.
+    partial void Construct();
+
+    // Add any OnLaunched customization here.
+    partial void LaunchCompleted(LaunchActivatedEventArgs e);
+  }
 }
